8ae880e34f657c6dcf88dd5d5d4b68304fa7e0e7
Jovan Djukic <Jovan Djukic>
2016-09-06 23:39:46 +0200

    added progress bars

--- clock.js
+++ clock.js
@@ -6,6 +6,16 @@ function printTime() {
 
     var output = document.getElementById("output");
     output.innerHTML = h + " : " + m + " : " + s;
+
+    var progHour = document.getElementById("progHour");
+    progHour.value = h;
+
+    var progMin = document.getElementById("progMin");
+    progMin.value = m;
+
+    var progSec = document.getElementById("progSec");
+    progSec.value = s;
 }
 
 setInterval(printTime, 1000);
+
